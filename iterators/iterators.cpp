6fee23cd5c3e8de0f2f81e3acb6facd581997898
Adrian <Adrian@DESKTOP-H4LLF2K>
2022-02-24 11:17:30 +0100

    Dodaj pliki projektów.

---
+++ iterators/iterators.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <vector>
+
+template<typename T>
+void print_collection_buff(const T& elem, size_t beg, size_t end)
+{
+	auto beggining = elem.begin() + beg;
+	auto ending = elem.end() - end;
+
+	while (beggining != ending)
+	{
+		std::cout << *beggining << std::endl;
+		++beggining;
+	}
+}
+template<typename T>
+void print_reverse(const T& elem)
+{
+	auto start = elem.rend();
+	auto end = elem.rbegin();
+
+	while (end != start)
+	{
+		std::cout << *end << std::endl;
+		++end;
+	}
+}
+
+template<typename T>
+void print_const(const T& elem)
+{
+	auto start = elem.cbegin();
+
+	while (start != elem.cend())
+	{
+		std::cout << *start << std::endl;
+		++start;
+	}
+}
+
+int main()
+{
+	std::vector<int> first_vec{ 1,2,3,4 };
+
+	print_reverse(first_vec);
+	print_const(first_vec);
+}
